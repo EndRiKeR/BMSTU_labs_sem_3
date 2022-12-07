fbf39f9ea4df3208a5bb0a290a54cc2ed49ddd70
Selemeniv Roman <solomenckov.petya@ya.ru>
2022-12-07 11:02:24 +0300

    save before py

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+int getArray(int* arr, int arrSize);
+
+int main()
+{
+    int arr[10];
+    int arrSize = 10;
+
+    getArray(arr, arrSize);
+}
+
+int getArray(int* arr, int arrSize)
+{
+    int rc = 0;
+    char inBuff[50];
+    int buff;
+    arr = (int*)malloc(sizeof(int) * arrSize);
+    for (int i = 0; i < arrSize; i++) {
+        printf("Введите %d символ", i);
+        scanf_s("%s", inBuff, 49);
+        printf("%s", inBuff);
+        if (inBuff == "0") {
+            arr[i] = 0;
+        }
+        else {
+            buff = atoi(inBuff);
+            if (!buff) {
+                rc = 1;
+                break;
+            }
+            else {
+                arr[i] = buff;
+            }
+        }
+    }
+    return rc;
+}
+
